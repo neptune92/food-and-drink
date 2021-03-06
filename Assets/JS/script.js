44a826c129ea75eaa90df7fdb5eb341c12b76048
Madison Vega <madison.r.vega@gmail.com>
2021-03-06 13:34:21 -0600

    added more notes

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,12 +11,16 @@
 
 // MVP
 // search barx2
+    // dropdown with different criteria to select
 // submit buttonx2
 // variables
     // search input value
     // variables to get handles on html elements
+    // assign classes to html elements 
 // functions
     // function for api call
-    // event handler for submit
+    // event handler for submit button
     // display results function
+    // local storage
 // basic styling
+
