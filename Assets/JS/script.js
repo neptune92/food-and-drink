ee7672793f18ec4a0de1c3388521639518d417cf
Madison Vega <madison.r.vega@gmail.com>
2021-03-06 11:21:09 -0600

    made changes to html

---
+++ Assets/JS/script.js
@@ -0,0 +1,16 @@
+// what did i get done
+    // nothing
+// what are we going to get done
+    // brainstorm
+    // pseudocode
+    // get ideas down
+    // get basic elements in
+// do i have any impediments
+    // time constraints
+
+
+
+// search bar
+// submit button
+// 
+
